2241a026bf912fff6e328648907a806b1530a23c
Andrew Ashmankevich <Ashmankevich.a@gmail.com>
2023-03-08 22:09:56 +0300

    fix search function

--- src/scripts/search.js
+++ src/scripts/search.js
@@ -1,14 +1,21 @@
+import { renderTemplate } from ".";
+
 function search() {
-  let value = this.value.trim();
+  let value = this.value.trim().toLowerCase();
   let items = document.querySelectorAll(".todo-item");
   if (value != "") {
     items.forEach((elem) => {
-      elem.innerText.search(value) == -1 ? (elem.style.display = "none") : null;
+      elem.textContent.toLowerCase().search(value) == -1
+        ? (elem.style.display = "none")
+        : null;
     });
   } else {
     items.forEach((elem) => {
-      elem.innerText.search(value) == -1 ? (elem.style.display = "flex") : null;
+      elem.textContent.toLowerCase().search(value) == -1
+        ? (elem.style.display = "flex")
+        : null;
     });
+    renderTemplate();
   }
 }
 
