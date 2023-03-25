5111abbb80ae03cb59984f8314d9989f0fda41ba
Andrew Ashmankevich <Ashmankevich.a@gmail.com>
2023-03-25 12:22:41 +0300

    fix date function

--- src/scripts/components/date.js
+++ src/scripts/components/date.js
@@ -1,11 +1,32 @@
 function getDate() {
   let date = new Date();
   let year = date.getFullYear();
-  let month = date.getMonth();
+  let monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = monthNames[date.getMonth()];
   let day = date.getDate();
   let hour = date.getHours();
   let min = date.getMinutes();
-  return `${day}/${month}/${year} - ${hour}:${min}`;
+
+  day < 10 ? (day = " " + day) : day;
+  hour < 10 ? (hour = "0" + hour) : hour;
+  min < 10 ? (min = "0" + min) : min;
+
+  let fullDate = day + " " + month + " " + year + " - " + hour + ":" + min;
+
+  return fullDate;
 }
 
 export { getDate };
